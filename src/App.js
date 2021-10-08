0d08a260aee126bf843409152f49cbad19c55a5f
okoli praiz <okolipraiz.com>
2021-10-08 21:56:38 +0100

    Updated Project

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Button>first React app</Button>
+        <Button>first React app loading</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
